344132a79a61ff4ac538c6970b3258d48ee0520e
unknown <jd.david.serrudo.h133@gmail.com>
2023-10-27 17:27:07 -0400

    huierfiueufh

--- prueba-backend/src/productos/entities/producto.entity.ts
+++ prueba-backend/src/productos/entities/producto.entity.ts
@@ -1,1 +1,13 @@
-export class Producto {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('productos')
+export class Producto {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 30, nullable: false})
+    descripcion: string;
+
+    @Column({ type: 'varchar', length: 40, nullable: false})
+    unidad: string;
+}
